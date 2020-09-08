bb0e9d7f998d380e1d3323ea10a4412402dc0cc8
Xanhenha <xanhenha@gmail.com>
2020-09-08 09:06:57 -0300

    implementando a edicao de produtos`

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  productinho: Product;
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readByID(id).subscribe(product => {
+      this.productinho = product;
+    })
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.productinho).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!');
+      this.router.navigate(['/products']);
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
